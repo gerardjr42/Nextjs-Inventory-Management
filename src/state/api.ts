a12f064fe0025bcb7da9951f2cbdedc00793a335
gerardjr42 <ggarciajr12@gmail.com>
2024-08-08 05:29:29 -0400

    create redux store

---
+++ src/state/api.ts
@@ -0,0 +1,10 @@
+// Create boiler plate to make API calls in Redux js and toolkit
+import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query";
+
+export const api = createApi({
+  baseQuery: fetchBaseQuery({ baseUrl: process.env.NEXT_PUBLIC_API_BASE_URL }),
+  reducerPath: "api",
+  tagTypes: [],
+  endpoints: (build) => ({}),
+});
+export const {} = api;
