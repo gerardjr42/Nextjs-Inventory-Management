ec322c9fbeef68613185d7763f351bfff9320178
gerardjr42 <ggarciajr12@gmail.com>
2024-08-08 04:15:17 -0400

    create navbar component

---
+++ src/app/DashboardWrapper.tsx
@@ -0,0 +1,15 @@
+import Navbar from "./(components)/Navbar";
+
+const DashboardWrapper = ({ children }: { children: React.ReactNode }) => {
+  return (
+    <div className="light bg-gray-50 text-gray-900 w-full flex min-h-screen">
+      Sidebar
+      <main className="flex flex-col w-full h-full py-7 px-9 bg-gray-50 md:pl-24">
+        <Navbar />
+        {children}
+      </main>
+    </div>
+  );
+};
+
+export default DashboardWrapper;
